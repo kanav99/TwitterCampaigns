d30d292d2ee237f8097855417c2bb4e3870b6682
Kanav Gupta <kanav0610@gmail.com>
2020-06-16 18:07:52 +0530

    Initial Commit

---
+++ ByeTwitter/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  ByeTwitter
+//
+//  Created by Kanav Gupta on 16/06/20.
+//  Copyright © 2020 Kanav Gupta. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
